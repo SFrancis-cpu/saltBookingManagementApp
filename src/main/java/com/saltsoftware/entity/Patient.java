beb7b0233428a3cf86a3e020465ab8f3c20dfa8e
BathiNtshinga <60979642+BathiNtshinga@users.noreply.github.com>
2020-07-05 18:12:11 +0200

    First commit

---
+++ src/main/java/com/saltsoftware/entity/Patient.java
@@ -0,0 +1,55 @@
+package com.saltsoftware.entity;
+
+
+public class Patient{
+    private String patientID;
+    private String patientName;
+
+    private Patient (Builder builder){
+        this.patientID = builder.patientID;
+        this.patientName = builder.patientName;
+
+    }
+
+    public String getPatientID() {
+        return patientID;
+    }
+
+    public String getPatientName() {
+        return patientName;
+    }
+
+    @Override
+    public String toString() {
+        return "Patient{" +
+                "patientID='" + patientID + '\'' +
+                ", patientName='" + patientName + '\'' +
+                '}';
+    }
+
+    public static class Builder{
+
+        private String patientID;
+        private String patientName;
+
+        public Builder setPatientID(String patientID){
+            this.patientID = patientID;
+            return this;
+        }
+
+        public Builder setPatientName(String patientName){
+            this.patientName = patientName;
+            return this;
+        }
+        public Builder copy(Patient patient){
+            this.patientID = patient.patientID;
+            this.patientName = patient.patientName;
+            return this;
+        }
+        public Patient build(){
+            return new Patient(this);
+        }
+    }
+}
+
+
