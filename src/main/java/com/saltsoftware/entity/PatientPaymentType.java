af21a82aa223eab093bf60348955d8d22a086eec
arendshp <42059316+arendshp@users.noreply.github.com>
2020-07-05 17:05:29 +0200

    Commit PatientPaymentType class, with builder design pattern

---
+++ src/main/java/com/saltsoftware/entity/PatientPaymentType.java
@@ -0,0 +1,43 @@
+package com.saltsoftware.entity;
+
+public class PatientPaymentType {
+    private final String paymentTypeID;
+    private final String paymentDescrip;
+
+    //constructor
+    public PatientPaymentType(UserBuilder builder) {
+        this.paymentTypeID = builder.paymentTypeID;
+        this.paymentDescrip = builder.paymentDescrip;
+    }
+
+    //inner builder class
+    public static class UserBuilder{
+        private final String paymentTypeID;
+        private String paymentDescrip;
+
+        //inner class constructor
+        public UserBuilder(String paymentTypeID) {
+            this.paymentTypeID = paymentTypeID;
+        }
+
+        //getters
+        public UserBuilder paymentDescrip(String paymentDescrip){
+            this.paymentDescrip = paymentDescrip;
+            return this;
+        }
+
+        //builder method
+        public PatientPaymentType build(){
+            return new PatientPaymentType(this);
+        }
+    } //end of inner class
+
+    @Override
+    public String toString() {
+        return "PatientPaymentType{" +
+                "paymentTypeID='" + paymentTypeID + '\'' +
+                ", paymentDescrip='" + paymentDescrip + '\'' +
+                ", records=" +
+                '}';
+    }
+}
