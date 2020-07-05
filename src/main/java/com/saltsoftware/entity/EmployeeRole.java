b16b1ee2ae8465b803da0957f3c64cfbc9d941ef
Sakeena Francis <sakeenafrancis24@gmail.com>
2020-07-05 16:14:30 +0200

    first commit

---
+++ src/main/java/com/saltsoftware/entity/EmployeeRole.java
@@ -0,0 +1,99 @@
+package com.saltsoftware.entity;
+
+public class EmployeeRole {
+
+       private int empID, roleID;
+
+
+
+        private EmployeeRole(Builder builder) {
+
+            this.empID = builder.empID;
+
+            this.roleID = builder.roleID;
+
+        }
+
+
+
+        public int getEmpID() {
+
+            return empID;
+
+        }
+
+
+
+        public int getRoleID() {
+
+            return roleID;
+
+        }
+
+
+
+        @Override
+
+        public String toString() {
+
+            return "EmployeeRole{" +
+
+                    "empID=" + empID +
+
+                    ", roleID=" + roleID +
+
+                    '}';
+
+        }
+
+
+
+        public static class Builder {
+
+            private int empID, roleID;
+
+
+
+            public Builder setEmpID(int empID) {
+
+                this.empID = empID;
+
+                return this;
+
+            }
+
+
+
+            public Builder setRoleID(int roleID) {
+
+                this.roleID = roleID;
+
+                return this;
+
+            }
+
+
+
+            public Builder copy(EmployeeRole employeeRole){
+
+                this.empID = employeeRole.empID;
+
+                this.roleID = employeeRole.roleID;
+
+                return this;
+
+            }
+
+
+
+            public EmployeeRole build(){
+
+                return new EmployeeRole(this);
+
+            }
+
+        }
+
+    }
+
+
