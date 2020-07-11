41a77b45e8cdef2a945565abb9ac0466a7d4be3f
AFrank <abduragmaanfrank@gmail.com>
2020-07-12 01:31:17 +0200

    My second commit for Factory

--- src/main/java/com/saltsoftware/factory/EmployeeFactory.java
+++ src/main/java/com/saltsoftware/factory/EmployeeFactory.java
@@ -3,6 +3,12 @@ package com.saltsoftware.factory;
 import com.saltsoftware.entity.Employee;
 import com.saltsoftware.util.GenericHelper;
 
+/**
+ * Created by :Abduragmaan Frank
+ * Student no: 217009069
+ * Desc: Create Employee Factory
+ */
+
 public class EmployeeFactory {
 
     public static Employee  createEmployee(String empName, String empLastName){
