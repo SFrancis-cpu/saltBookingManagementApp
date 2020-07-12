eb4a28b3f28a65302f81d9604b5b8a7324e83fdf
Sakeena Francis <sakeenafrancis24@gmail.com>
2020-07-12 14:11:04 +0200

    Phase two - Create Factory, Create UTIL, and create TEST cases

--- src/main/java/com/saltsoftware/entity/EmployeeRole.java
+++ src/main/java/com/saltsoftware/entity/EmployeeRole.java
@@ -3,7 +3,7 @@ package com.saltsoftware.entity;
 /* @Author: Sakeena Francis
  *   StudentNumber : 215006097
  *   This is a EmployeeRole class
- *   change RoleType
+ *   change RoleTypes
  * */
 
 public class EmployeeRole {
@@ -11,91 +11,47 @@ public class EmployeeRole {
        private String empID, roleID;
 
 
-
         private EmployeeRole(Builder builder) {
-
             this.empID = builder.empID;
-
             this.roleID = builder.roleID;
-
         }
-
-
 
         public String getEmpID() {
-
             return empID;
-
         }
-
-
 
         public String getRoleID() {
-
             return roleID;
-
         }
-
-
 
         @Override
-
-        public String toString() {
-
+       public String toString() {
             return "EmployeeRole{" +
-
                     "empID=" + empID +
-
                     ", roleID=" + roleID +
-
                     '}';
-
         }
 
-
-
         public static class Builder {
-
             private String empID, roleID;
-
-
-
             public Builder setEmpID(String empID) {
-
                 this.empID = empID;
-
                 return this;
-
             }
-
-
 
             public Builder setRoleID(String roleID) {
-
                 this.roleID = roleID;
-
                 return this;
-
             }
-
-
 
             public Builder copy(EmployeeRole employeeRole){
-
                 this.empID = employeeRole.empID;
-
                 this.roleID = employeeRole.roleID;
-
                 return this;
-
             }
 
-
-
             public EmployeeRole build(){
-
                 return new EmployeeRole(this);
-
             }
 
         }
