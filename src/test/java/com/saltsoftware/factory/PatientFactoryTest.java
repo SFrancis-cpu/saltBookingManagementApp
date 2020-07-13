9360556e324b7b19c7af87fde36d9183c2fb014c
BathiNtshinga <60979642+BathiNtshinga@users.noreply.github.com>
2020-07-13 18:02:44 +0200

    Initial commit

--- src/test/java/com/saltsoftware/factory/PatientFactoryTest.java
+++ src/test/java/com/saltsoftware/factory/PatientFactoryTest.java
@@ -5,22 +5,42 @@
  * */
 
 package com.saltsoftware.factory;
-
 import com.saltsoftware.entity.Patient;
 import org.junit.Assert;
 import org.junit.Before;
-import junit.framework.TestCase;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class PatientFactoryTest {
 
-    @Test
+    @Before
     public void createPatient() {
         Patient patient1 = PatientFactory.createPatient("Bathi", "Ntshinga");
-        assertEquals(patient1.getPatientName(),patient1.getPatientName());
+        assertSame(patient1.getPatientID(), patient1.getPatientSurname());
     }
+
+   @Test
+   /*Testing two objects for identity and equality patient1 and patient2*/
+   public void testCreatePatient() {
+       Patient patient1 = PatientFactory.createPatient("Bathi","Ntshinga");
+       Patient patient2 = PatientFactory.createPatient("Noluthando","Nqwelo");
+
+       assertSame(patient1.getPatientName(),patient2.getPatientName());
+
+       assertEquals(patient1.getPatientID(),patient2.getPatientID());
+   }
+    /*Test identity on patient Surname*/
+    @Test
+    public void testIdentity(){
+
+        Patient patient1 = PatientFactory.createPatient("Bathi","Ntshinga");
+        Patient patient2 = PatientFactory.createPatient("Noluthando","Nqwelo");
+
+        assertSame(patient1.getPatientSurname(), patient2.getPatientSurname());
+        assertEquals(patient1.getPatientSurname(), patient2.getPatientSurname());
+    }
+
 
 
 }
