b1121b6c3cd89ebc346518fb2f3d7520d5066bf7
Fish84 <craigacarr@gmail.com>
2020-07-13 15:07:40 +0200

    Commit for BookingScheduleFactory and BookingScheduleFactoryTest

--- src/main/java/com/saltsoftware/util/GenericHelper.java
+++ src/main/java/com/saltsoftware/util/GenericHelper.java
@@ -7,13 +7,12 @@ package com.saltsoftware.util;
  * date: 3 July 2020
  **/
 import java.util.UUID;
+
 /*
  Author: Sakeena Francis
  Description: Create utility package that generates ID's
  Date: 11-Jul-2020
 */
-
-import java.util.UUID;
 
 public class GenericHelper {
 
@@ -23,6 +22,16 @@ public class GenericHelper {
 
     //Create utility package that generates Receipt Number Randomly
     public static String generateReceiptNumber(){
+        return UUID.randomUUID().toString();
+    }
+
+    //Create utility package that generates Booking Number Randomly
+    /**
+     * Created by :Craig Carr AKA Fish84
+     * Student no: 204111307
+     * Desc: generates bookingID
+     */
+    public static String generateBookingID(){
         return UUID.randomUUID().toString();
     }
 
