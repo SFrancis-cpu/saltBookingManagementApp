a25d9d316ad8be7c2667e5ae321ecb24b8c0549b
Sakeena Francis <sakeenafrancis24@gmail.com>
2020-07-14 17:44:01 +0200

    Update to EmployeeRoleFactoryTest

--- src/test/java/com/saltsoftware/factory/EmployeeRoleFactoryTest.java
+++ src/test/java/com/saltsoftware/factory/EmployeeRoleFactoryTest.java
@@ -12,16 +12,33 @@ import static org.junit.Assert.*;
 
     public class EmployeeRoleFactoryTest {
 
+        private EmployeeRole emp1 = EmployeeRoleFactory.createEmployeeRole();
+        private EmployeeRole emp2 = EmployeeRoleFactory.createEmployeeRole();
+
         @Test
         public void testCreateEmployeeRole() {
-            EmployeeRole emp1 = EmployeeRoleFactory.createEmployeeRole();
-            EmployeeRole emp2 = EmployeeRoleFactory.createEmployeeRole();
+        }
+
+        @Test
+        public void testEquality() {
 
             //Equality test
             assertEquals(emp1.getEmpID(),emp1.getEmpID());
             assertEquals(emp2.getRoleID(),emp2.getRoleID());
         }
 
+        @Test
+        public void testNotNull() {//NotNull test
+            assertNotNull(emp1.getEmpID());
+            assertNotNull(emp2.getRoleID());
+        }
+
+        @Test
+        public void testIdentity(){
+            //Identity test
+            assertSame(emp1.getEmpID(), emp1.getEmpID());
+            assertSame(emp2.getRoleID(), emp2.getRoleID());
+        }
 
     }
 
