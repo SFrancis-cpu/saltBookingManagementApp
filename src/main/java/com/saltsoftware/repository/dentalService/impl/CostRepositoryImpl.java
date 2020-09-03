dda4ed6e40d60a22bc9e9114e41db1d10e424a9c
Junadef <junade.frizlar@gmail.com>
2020-09-03 18:12:45 +0200

    Fixed

--- src/main/java/com/saltsoftware/repository/dentalService/impl/CostRepositoryImpl.java
+++ src/main/java/com/saltsoftware/repository/dentalService/impl/CostRepositoryImpl.java
@@ -4,59 +4,61 @@ import com.saltsoftware.entity.dentalService.Cost;
 import com.saltsoftware.repository.dentalService.CostRepository;
 
 import java.util.HashSet;
-import java.util.*;
-
-
-/* @Author: Junade Frizlar
- *   StudentNumber : 208046402
- *   This is a cost repository.
- * */
+import java.util.Set;
+/**
+ * Created by :Junade Frizlar
+ * Student no: 208046402
+ * Desc: Create Cost Repository
+ */
 
 public class CostRepositoryImpl implements CostRepository {
     private Set<Cost>CostDB;
+
     //Constructor
     public CostRepositoryImpl(){
         this.CostDB = new HashSet<>();
     }
 
-    // This method creates cost
+
+    // This method creates a Employee
     @Override
     public Cost create(Cost cost) {
         this.CostDB.add(cost);
         return cost;
     }
-
-    //This method reads from Cost database
+    //This method reads from Employee database
     @Override
-    public Set<Cost> read(String CostID) {
+    public Cost read(String empId) {
         Cost cost = null;
-        for(Cost c : this.CostDB){
-            if(c.getCostID().equalsIgnoreCase(costId)){
+        for(Cost c: this.CostDB){
+            if(c.getCostID().equalsIgnoreCase(cost.costID)){
                 cost = c;
                 break;
             }
         }
         return cost;
-
-        public Cost update(Cost cost) {
-            Set<Cost> oldCost = read(cost.getCostID());
-            if(oldCost != null){
-                this.CostDB.remove(oldCost);
-                this.CostDB.add(cost);
-            }
-            return cost;
-        }
-
-            //This method removes and cost object
-        public void delete(String CostID) {
-            Cost cost = read(CostID);
-            if(cost != null);
-            this.CostDB.remove(cost);
-
-        }
-        @Override
-        public Set<Cost> getAll() {
-            return this.CostDB;
-        }
     }
+
+    //This method updates an existing employee
+    public Cost update(Cost cost) {
+        Cost oldCost = read(cost.getCostID());
+        if(oldCost != null){
+            this.CostDB.remove(oldCost);
+            this.CostDB.add(cost);
+        }
+        return cost;
+    }
+
+    //This method removes and employee object
+    public void delete(String CostID) {
+        Cost cost = read(CostID);
+        if(cost != null);
+        this.CostDB.remove(cost);
+
+    }
+    @Override
+    public Set<Cost> getAll() {
+        return this.CostDB;
+    }
+}
 
