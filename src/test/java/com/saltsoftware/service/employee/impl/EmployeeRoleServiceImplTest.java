266b8af4a77dc9f838e4babeecd4cc3949bce165
SFrancis-cpu <Sakeenafrancis24@gmail.com>
2020-09-09 10:26:30 +0200

    re-create Employee Service Impl Test Case

---
+++ src/test/java/com/saltsoftware/service/employee/impl/EmployeeRoleServiceImplTest.java
@@ -0,0 +1,65 @@
+package com.saltsoftware.service.employee.impl;
+
+import com.saltsoftware.entity.employee.EmployeeRole;
+import com.saltsoftware.factory.employee.EmployeeRoleFactory;
+import com.saltsoftware.service.employee.EmployeeRoleService;
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runners.MethodSorters;
+
+import java.util.Set;
+
+import static org.junit.Assert.*;
+
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class EmployeeRoleServiceImplTest {
+
+    private static EmployeeRoleService service = EmployeeRoleServiceImpl.getService();
+    private static EmployeeRole employeeRole = EmployeeRoleFactory.buildEmployeeRole("Ma885");
+
+
+    //GetAll from the DB
+    @Test
+    public void d_getAll() {
+        Set<EmployeeRole> employeeRoles = service.getAll();
+        assertEquals(1, employeeRoles.size());
+        System.out.println(service.getAll());
+    }
+
+
+    //Create Method
+    @Test
+    public void a_create() {
+        EmployeeRole created = service.create(employeeRole);
+        assertEquals(employeeRole.getEmpID(), created.getEmpID());
+        System.out.println("Create: " + created);
+    }
+
+
+    //Read Method
+    @Test
+    public void b_read() {
+        EmployeeRole read = service.read(employeeRole.getEmpID());
+        assertEquals(employeeRole.getEmpID(), read.getEmpID());
+        System.out.println("Read: " + read);
+    }
+
+
+    //Update Method
+    @Test
+    public void c_update() {
+        //EmployeeRole updated = new EmployeeRole.Builder().setEmpID("").build();
+        EmployeeRole updated = new EmployeeRole.Builder().copy(employeeRole).setEmpID("588").build();
+        service.update(updated);
+        assertNotEquals(employeeRole.getEmpID(), updated.getEmpID());
+        System.out.println("Update: " + updated);
+    }
+
+
+    //Delete Method
+    @Test
+    public void e_delete() {
+        service.delete(employeeRole.getEmpID());
+    }
+
+}
