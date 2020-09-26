a3ac40f19d19657d96093650383ca4a961f3920b
AFrank <abduragmaanfrank@gmail.com>
2020-09-26 23:24:45 +0200

    Employee Controller wip

---
+++ src/main/java/com/saltsoftware/controller/employee/EmployeeController.java
@@ -0,0 +1,58 @@
+package com.saltsoftware.controller.employee;
+
+
+import com.saltsoftware.entity.employee.Employee;
+import com.saltsoftware.factory.employee.EmployeeFactory;
+import com.saltsoftware.service.employee.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Set;
+
+/*
+    Author: Abduragmaan Frank
+    Student no: 217009069
+    Desc: Employee Controller
+ */
+
+@RestController
+@RequestMapping("/employee")
+public class EmployeeController {
+
+    @Autowired
+  private EmployeeService employeeService;
+
+    //Business Logic
+
+
+   @PostMapping("/create")
+    public Employee create(@RequestBody Employee employee){
+       // return employee;
+       employee = EmployeeFactory.createEmployee(employee.getEmpName(),employee.getEmpLastName());
+       return employeeService.create(employee);
+    }
+
+    // get all services
+    @GetMapping("/all")
+    public Set<Employee> getAll(){
+       return employeeService.getAll();
+    }
+
+    // read method
+    @GetMapping("/read/{id}")
+    public Employee read(@PathVariable String id){
+       return employeeService.read(id);
+    }
+    //Update method
+    @PostMapping("/update")
+    public Employee update(@RequestBody Employee employee){
+       return employeeService.update(employee);
+    }
+    //Delete method
+    @DeleteMapping("/delete/{id}")
+    public boolean delete(@PathVariable String id){
+       return employeeService.delete(id);
+
+    }
+
+}
