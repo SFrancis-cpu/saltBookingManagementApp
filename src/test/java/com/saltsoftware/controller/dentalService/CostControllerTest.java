6d0da5d88a8362f7c957f898dd121995c81ba639
Junadef <junade.frizlar@gmail.com>
2020-09-27 20:57:34 +0200

    Test cases updated Commit

--- src/test/java/com/saltsoftware/controller/dentalService/CostControllerTest.java
+++ src/test/java/com/saltsoftware/controller/dentalService/CostControllerTest.java
@@ -1,9 +1,13 @@
 package com.saltsoftware.controller.dentalService;
 
 import com.saltsoftware.entity.dentalService.Cost;
+import com.saltsoftware.factory.dentalService.CostFactory;
 import org.junit.Assert;
+import org.junit.FixMethodOrder;
 import org.junit.Test;
+import org.junit.jupiter.params.shadow.com.univocity.parsers.common.StringCache;
 import org.junit.runner.RunWith;
+import org.junit.runners.MethodSorters;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
@@ -11,70 +15,59 @@ import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.web.client.RestClientException;
-import org.springframework.test.context.junit4.SpringRunner.*;
+
+
 
 import static org.junit.Assert.*;
 
+//Random port to avoid an already used port
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @RunWith(SpringRunner.class)
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 
-public class CostControllerTest<cost> {
+public class CostControllerTest {
 
     @Autowired
     private TestRestTemplate restTemplate;
-    private String baseURL = "http://localhost:8080/cost/";
-    private java.lang.Object cost;
-    private java.lang.Object Cost;
-    private java.lang.String myURL;
+    private  String baseURL = "http://localhost:8080/cost";
 
-    public CostControllerTest(TestRestTemplate restTemplate) {
-        this.restTemplate = restTemplate;
-    }
-
-    //create
     @Test
-    public void a_create() {
-        java.lang.String myURL = "";
-        String URL = myURL + "create";
-
-        System.out.println("Post data"+ cost);
-        ResponseEntity<Cost> postResponse = restTemplate.postForEntity(myURL,cost,Cost.class);
-        assertNotNull(postResponse);
-        Assert.assertEquals(Cost, java.util.Objects.requireNonNull(postResponse.getBody()).getCostID());
+    public void create() {
     }
+
+    /*@Test
+    public void getAll()
+    {
+        String url = baseURL + "all";
+        System.out.println("url:" + url);
+        HttpHeaders headers = new HttpHeaders();
+        HttpEntity<String> entity = new HttpEntity<>(null,headers);
+        ResponseEntity<String> response = restTemplate.exchange(url, HttpMethod.GET, entity, String.class);
+        System.out.println(response);
+        System.out.println(response.getBody());
+        assertNotNull(response);
+    }*/
 
     @Test
     public void read() {
     }
-
 
     @Test
     public void update() {
     }
 
     @Test
-    public void e_delete() {
-        String url = myURL +"delete/"+ cost;
+    public void delete()
+    {
+        String url = baseURL +"/cost"+restTemplate;
         System.out.println("URL: "+url);
         try {
-            restTemplate.delete(baseURL);
+            restTemplate.delete(url);
         } catch (RestClientException e) {
-            e.contains(Cost.class);
+            e.printStackTrace();
         }
-    }
-
-    //Test case for getAll method
-    @Test
-    public void d_getAll() {
-        java.lang.String myURL = "";
-        String url = myURL + "all";
-        System.out.println("URL "+ url);
-        HttpHeaders headers = new HttpHeaders();
-        HttpEntity<String> entity = new HttpEntity(null,headers);
-        ResponseEntity<String> response = restTemplate.exchange(url, HttpMethod.GET,entity,String.class);
-        System.out.println(response);
-        System.out.println(response.getBody());
-        assertNotNull(response);
     }
 }
