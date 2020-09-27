99dc32667ce01b6cebb865842b80630a3cad0312
Junadef <junade.frizlar@gmail.com>
2020-09-27 21:13:22 +0200

    Cost controller Commit

--- src/test/java/com/saltsoftware/controller/dentalService/CostControllerTest.java
+++ src/test/java/com/saltsoftware/controller/dentalService/CostControllerTest.java
@@ -15,7 +15,6 @@ import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.web.client.RestClientException;
 
