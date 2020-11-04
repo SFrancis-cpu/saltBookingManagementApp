6e3a508b625475b6ed500bed0b8e5c76289a070f
Junadef <junade.frizlar@gmail.com>
2020-11-04 15:29:19 +0200

    Final commit

--- src/main/java/com/saltsoftware/service/dentalService/impl/CostServiceImpl.java
+++ src/main/java/com/saltsoftware/service/dentalService/impl/CostServiceImpl.java
@@ -4,13 +4,13 @@ import com.saltsoftware.service.dentalService.impl.CostServiceImpl;
 import com.saltsoftware.entity.dentalService.Cost;
 import com.saltsoftware.repository.dentalService.CostRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.annotation.MergedAnnotationCollectors;
 import org.springframework.stereotype.Service;
 /*
  * @author name: Junade Frizlar
  * student number: 208046402
  **/
 import java.util.Set;
+import java.util.stream.Collectors;
 
 @Service
 public class CostServiceImpl implements CostService {
