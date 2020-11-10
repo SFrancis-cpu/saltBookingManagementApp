c8bbfddad22814fb68ec594bf0d397cc3dbb1a1d
BathiNtshinga <60979642+BathiNtshinga@users.noreply.github.com>
2020-11-10 17:27:27 +0200

    Initial Commit

--- src/test/java/com/saltsoftware/controller/patient/PatientControllerTest.java
+++ src/test/java/com/saltsoftware/controller/patient/PatientControllerTest.java
@@ -2,7 +2,8 @@ package com.saltsoftware.controller.patient;
 
 /* author: Bathi Ntshinga
  * std no:214198227
- * Description: Test Case for Domain Driven Patient Controller
+ * Date: 10/11/2020
+ * Description: Test Case for Application Security
  */
 
 
@@ -30,60 +31,73 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class PatientControllerTest {
 
+    private static Patient patient = PatientFactory.createPatient("Bathi","Ntshinga");
+    private static String SECURITY_USERNAME = "sma";
+    private static String SECURITY_PASSWORD = "password";
+
     @Autowired
     private TestRestTemplate restTemplate;
-    private static Patient patient = PatientFactory.createPatient("Bathi","Ntshinga");
 
     private String baseURL = "http://localhost:8080/patient/";
 
-    //Testing the creation of patientName
+    //Testing the creation of patientName for application security
+
     @Test
     public void a_create() {
-
         String url = baseURL + "create";
 
-        ResponseEntity<Patient> postResponse = restTemplate.postForEntity(url, patient, Patient.class);
-        Assert.assertEquals(patient.getPatientID(),postResponse.getBody().getPatientID());
-        System.out.println(postResponse.getBody());
-
+        System.out.println("Post data: "+ patient);
+        ResponseEntity<Patient> postResponse = restTemplate
+                .withBasicAuth(SECURITY_USERNAME,SECURITY_PASSWORD)
+                .postForEntity(url,patient,Patient.class);
+        assertNotNull(postResponse);
+        Assert.assertEquals(patient.getPatientName(),postResponse.getBody().getPatientName());
     }
-    // Test the read command
+    // Test the read command for application security
     @Test
     public void b_read() {
-        String url = baseURL + "read/"+ patient.getPatientName();
-        System.out.println("URL "+ url);
-        System.out.println(url);
-        ResponseEntity<Patient> getResponse = restTemplate.getForEntity(url,Patient.class);
-        System.out.println("this is response--> "+getResponse);
-
-
+        String url = baseURL + "read" + patient.getPatientID();
+        System.out.println("read " + patient);
+        ResponseEntity<Patient> responseEntity = restTemplate
+                .withBasicAuth(SECURITY_USERNAME,SECURITY_PASSWORD)
+                .getForEntity(url,Patient.class);
+        assertNotNull(responseEntity);
+        assertNotNull(responseEntity.getBody());
     }
+
     @Test
     public void c_update() {
-        Patient updated = new Patient.Builder().copy(patient).setPatientName("Bathi").build();
+        Patient updated = new Patient.Builder().copy(patient).setPatientID("AAA").setPatientID("AAA").build();
         String url = baseURL + "update";
-        ResponseEntity<Patient> responseEntity = restTemplate.postForEntity(url, updated, Patient.class);
+        ResponseEntity<Patient> responseEntity = restTemplate
+                .withBasicAuth(SECURITY_USERNAME,SECURITY_PASSWORD)
+                .postForEntity(url,updated, Patient.class);
         assertNotNull(responseEntity);
         assertNotNull(updated);
-        System.out.println("updated" + updated);
+        System.out.println("updated " + updated);
     }
 
     @Test
     public void d_getAll() {
         String url = baseURL + "all";
-        System.out.println("Get all "+ patient);
+        System.out.println("Get all" + patient);
         HttpHeaders headers = new HttpHeaders();
         HttpEntity<String> entity = new HttpEntity<>(null,headers);
-        ResponseEntity<String> responseEntity = restTemplate.exchange(url, HttpMethod.GET,entity,String.class);
+        ResponseEntity <String> responseEntity = restTemplate
+                .withBasicAuth(SECURITY_USERNAME,SECURITY_PASSWORD)
+                .exchange(url, HttpMethod.GET,entity, String.class );
         System.out.println(responseEntity.getBody());
         assertNotNull(responseEntity);
+
     }
 
-    //Testing the delete method
+    //Testing the delete method in application security
     @Test
     public void e_delete() {
-        String url = baseURL +"delete/"+ patient.getPatientName();
+        String url = baseURL +"delete/"+ patient.getPatientID();
         System.out.println("URL: "+url);
-        restTemplate.delete(url);
+        restTemplate
+                .withBasicAuth(SECURITY_USERNAME,SECURITY_PASSWORD)
+                .delete(url);
     }
 }
