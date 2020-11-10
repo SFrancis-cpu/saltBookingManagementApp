f552a99f26086c366e473021284fae577b5d52a1
Sakeena Francis <60943023+SFrancis-cpu@users.noreply.github.com>
2020-11-10 20:58:11 +0200

    Revert "Add Spring Security to Endpoints"

--- src/main/java/com/saltsoftware/controller/payment/PatientPaymentTypeController.java
+++ src/main/java/com/saltsoftware/controller/payment/PatientPaymentTypeController.java
@@ -49,7 +49,7 @@ public class PatientPaymentTypeController {
     }
 
     //delete
-    @DeleteMapping ("/delete ")
+    @DeleteMapping ("/delete/{id}")
     @ResponseBody
     public void delete(@PathVariable String id){
         paymentTypeService.delete(id);
--- src/test/java/com/saltsoftware/controller/payment/PatientPaymentTypeControllerTest.java
+++ src/test/java/com/saltsoftware/controller/payment/PatientPaymentTypeControllerTest.java
@@ -1,17 +1,21 @@
 package com.saltsoftware.controller.payment;
 
-
+import com.saltsoftware.entity.bookingSchedule.BookingSchedule;
 import com.saltsoftware.entity.payment.PatientPaymentType;
 import com.saltsoftware.factory.payment.PatientPaymentTypeFactory;
+import org.junit.Assert;
 import org.junit.FixMethodOrder;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.MethodSorters;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.http.*;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.junit.Assert.*;
@@ -23,13 +27,11 @@ import static org.junit.Assert.*;
 
 public class PatientPaymentTypeControllerTest {
 
-    private PatientPaymentType paymentTypeService = PatientPaymentTypeFactory.createPaymentType("EFT");
-    private static String SECURITY_USERNAME = "SUPER";
-    private static String SECURITY_PASSWORD = "5555";
+    private PatientPaymentType paymentTypeService = PatientPaymentTypeFactory.createPaymentType("Debit Card");
 
     @Autowired
     private TestRestTemplate restTemplate;
-    private String baseURL = "http://localhost:8989/paymenttype/";
+    private String baseURL = "http://localhost:8080/paymenttype/";
 
     //Test case for create method
     @Test
@@ -37,9 +39,7 @@ public class PatientPaymentTypeControllerTest {
         String url = baseURL + "create";
         System.out.println("URL: "+url);
         System.out.println("Post data: "+paymentTypeService);
-        ResponseEntity<PatientPaymentType> postResponse = restTemplate
-                .withBasicAuth(SECURITY_USERNAME, SECURITY_PASSWORD)
-                .postForEntity(url, paymentTypeService,PatientPaymentType.class);
+        ResponseEntity<PatientPaymentType> postResponse = restTemplate.postForEntity(url, paymentTypeService,PatientPaymentType.class);
         assertNotNull(postResponse);
         assertNotNull(postResponse.getBody());
         paymentTypeService = postResponse.getBody();
@@ -53,12 +53,10 @@ public class PatientPaymentTypeControllerTest {
     public void b_read() {
         String url = baseURL + "read/"+ paymentTypeService.getPaymentTypeID();
         System.out.println("URL "+ url);
-        ResponseEntity<PatientPaymentType> getResponse = restTemplate
-                .withBasicAuth(SECURITY_USERNAME, SECURITY_PASSWORD)
-                .getForEntity(url,PatientPaymentType.class);
+        ResponseEntity<PatientPaymentType> getResponse = restTemplate.getForEntity(url,PatientPaymentType.class);
         System.out.println("this is response--> "+getResponse);
         assertNotNull(getResponse);
-
+        //Assert.assertEquals(paymentTypeService,getResponse.getBody().getPaymentTypeID());
     }
 
     //Test case for update method
@@ -67,9 +65,7 @@ public class PatientPaymentTypeControllerTest {
         PatientPaymentType updated = new PatientPaymentType.Builder().copy(paymentTypeService).setDescrip("Credit Card").build();
         String url = baseURL + "update";
         System.out.println("url "+ url);
-        ResponseEntity<PatientPaymentType> response = restTemplate
-                .withBasicAuth(SECURITY_USERNAME, SECURITY_PASSWORD)
-                .postForEntity(url,updated,PatientPaymentType.class);
+        ResponseEntity<PatientPaymentType> response = restTemplate.postForEntity(url,updated,PatientPaymentType.class);
         assertEquals(updated.getPaymentTypeID(), response.getBody().getPaymentTypeID());
     }
 
@@ -79,9 +75,7 @@ public class PatientPaymentTypeControllerTest {
         String url = baseURL + "all";
         HttpHeaders headers = new HttpHeaders();
         HttpEntity<String> entity = new HttpEntity<>(null, headers);
-        ResponseEntity<String> response = restTemplate
-                .withBasicAuth(SECURITY_USERNAME, SECURITY_PASSWORD)
-                .exchange(url, HttpMethod.GET, entity, String.class);
+        ResponseEntity<String> response = restTemplate.exchange(url, HttpMethod.GET, entity, String.class);
         System.out.println(response);
         System.out.println(response.getBody());
     }
@@ -91,8 +85,6 @@ public class PatientPaymentTypeControllerTest {
     public void e_delete() {
         String url = baseURL + "delete/" + paymentTypeService.getPaymentTypeID();
         System.out.println("URL: "+ url);
-        restTemplate
-                .withBasicAuth(SECURITY_USERNAME, SECURITY_PASSWORD)
-                .delete(url);
+        restTemplate.delete(url);
     }
 }
