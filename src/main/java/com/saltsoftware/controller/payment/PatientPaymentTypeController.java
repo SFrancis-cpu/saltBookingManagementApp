f1b47f51ceb7dfc7abbbdbc227a8ff95648083bf
SFrancis-cpu <Sakeenafrancis24@gmail.com>
2020-11-15 17:39:33 +0200

    Patient commit

--- src/main/java/com/saltsoftware/controller/payment/PatientPaymentTypeController.java
+++ src/main/java/com/saltsoftware/controller/payment/PatientPaymentTypeController.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+
 import javax.validation.Valid;
 
 /*
