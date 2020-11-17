1100da7a9d11d81afa2697986975f4ca74c2a8fd
arendshp <42059316+arendshp@users.noreply.github.com>
2020-11-17 19:08:58 +0200

    Update end-point
    testing new end point, working !!!

--- src/main/java/com/saltsoftware/controller/payment/PatientPaymentTypeController.java
+++ src/main/java/com/saltsoftware/controller/payment/PatientPaymentTypeController.java
@@ -18,7 +18,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Set;
 
 @RestController
-@RequestMapping("/paymenttype")
+@RequestMapping("/saltBookingManagementApp/paymenttype")
 public class PatientPaymentTypeController {
 
     //injecting the constructor, calling payment type service
